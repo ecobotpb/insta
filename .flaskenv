f8277c075784a2b070e28eb6146654a5aee4c86b
ecobotpb <edcleyssonalves@gmail.com>
2024-03-21 23:25:39 -0300

    Add: add css to project

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=sorteio.py
+FLASK_DEBUG=1
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,37 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, flash, redirect
+
+
 
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    nome = "Mea Studios"
+@app.route('/index',defaults={'nome':'usuario'})
+
+@app.route('/index/<nome>', )
+def index(nome):
     dados = {'Profissão': 'Professor', 'Empresa':'Mea Inglês'}
     return render_template('index.html', nome=nome, dados=dados)
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# enviando dados via médoto GET
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    if usuario == 'admin' and senha =='senha123':
+        return "usuario: {} e senha: {}".format(usuario, senha)
+    else:
+        flash('Dados inválidos')
+        return redirect('/login')
+
+
+
